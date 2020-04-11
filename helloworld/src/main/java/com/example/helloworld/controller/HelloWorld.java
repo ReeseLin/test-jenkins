6dccfa1af5699b5f9e26e1c18b3f4cafa69b7a3e
林斌 <linbin@mail.39.net>
2020-04-11 15:38:59 +0800

    hello project

---
+++ helloworld/src/main/java/com/example/helloworld/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class HelloWorld {
+
+
+    @GetMapping("hello")
+    public String hello(){
+        return "hello world";
+    }
+
+}
